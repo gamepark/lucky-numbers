d25ab35dd0b2c210701e97bd829ca923ca9cb348
Tho33 <theo.gregorio@laposte.net>
2022-01-06 10:35:51 +0100

    rework faceupCloverrs Display to prevent bad display if there is too much clovers

--- app/src/clovers/FaceUpClovers.tsx
+++ app/src/clovers/FaceUpClovers.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react'
 import Clover from '@gamepark/lucky-number/material/Clover'
+import CloverColor from '@gamepark/lucky-number/material/CloverColor'
 import { usePlay } from '@gamepark/react-client'
 import { Draggable } from '@gamepark/react-components'
 import { CLOVER } from '../players/CloverDropArea'
@@ -14,15 +15,19 @@ type Props = {
 
 export default function FaceUpClovers({clovers, canDrag}: Props) {
   const play = usePlay()
-  return <>
+  const arrayExampleClovers = Array(15).fill({color:CloverColor.Green, number:2})
+  return (
+  <>
+
     {clovers.map((clover, index) => 
     <Draggable key={index} type={CLOVER} item={clover} canDrag={canDrag} drop={play} >
       <CloverImage clover={clover} css={[position(index), canDrag && canDragStyle]}/>
     </Draggable> )}
   </>
+  )
 }
 
 const position = (index: number) => css`
-  left: ${index * (cloverSize + 1) + 70}em;
-  top: ${70}em;
+  left: ${(index%6) * (cloverSize + 1) + 66}em;
+  top: ${Math.floor(index/6)*(cloverSize +1) + 70}em;
 `
