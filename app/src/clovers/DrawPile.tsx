7c930e10610b89e03d9bca7a68c6b5f924908cac
Tho33 <theo.gregorio@laposte.net>
2022-01-08 12:42:16 +0100

    change drawPile displaying method allowing to pick the clicked clover

--- app/src/clovers/DrawPile.tsx
+++ app/src/clovers/DrawPile.tsx
@@ -1,8 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react'
-import {drawCloverMove} from '@gamepark/lucky-number/moves/DrawClover'
-import {usePlay} from '@gamepark/react-client'
-import {useState} from 'react'
+import DrawClover, {drawCloverMove, DrawView, isDrawClover} from '@gamepark/lucky-number/moves/DrawClover'
+import PlaceClover from '@gamepark/lucky-number/moves/PlaceClover'
+import {Animation, useAnimation, usePlay, usePlayerId} from '@gamepark/react-client'
+import {useEffect, useState} from 'react'
 import {canDragStyle, cloverSize, headerHeight} from '../styles'
 import CloverImage from './CloverImage'
 
@@ -13,23 +14,53 @@ type Props = {
 
 export default function DrawPile({size, canDraw}: Props) {
   const play = usePlay()
-  const [positions] = useState<CssPosition[]>([...new Array(size)].map(() => ({radius: Math.random(), direction: Math.random(), rotation: Math.random()})))
+  const playerId = usePlayerId()
+  const [positions, setPositions] = useState<(CssPosition)[]>([...new Array(size)].map(() => ({radius: Math.random(), direction: Math.random(), rotation: Math.random()})))
+  const [indexDrew, setIndexDrew] = useState<number>(-1)
+  const drawCloverAnimation = useAnimation<DrawView>(animation => isDrawClover(animation.move))
+
+  function playDrawMove(index:number){
+    play(drawCloverMove, {delayed: true})
+    setIndexDrew(index)
+    const newArray = [...positions]
+    newArray[index] = {direction:null,radius:null,rotation:null}
+    setPositions(newArray)
+  }
+
+  useEffect(() => {   // We need to remove a clover when other players draw one, which is obviously not guaranted by onClick event
+    if(drawCloverAnimation !== undefined && drawCloverAnimation.action.playerId !== playerId){
+      const newArray = [...positions]
+      newArray[newArray.filter(isPositionned).length-1] = {direction:null,radius:null,rotation:null}
+      setPositions(newArray)
+    }
+  },[drawCloverAnimation?.duration])
+
+
+  
   return <>
-    {[...new Array(size)].map((_, index) =>
-      <CloverImage key={index} onClick={canDraw ? () => play(drawCloverMove, {delayed: true}) : undefined}
-                   css={[style(positions[index]), canDraw && canDragStyle]}/>
+    {positions.map((cssPos, index) =>
+      isPositionned(cssPos) && <CloverImage key={index} onClick={canDraw ? () => playDrawMove(index) : undefined}
+                   css={[style(positions[index]), canDraw && canDragStyle, isCloverDrew(index, drawCloverAnimation, indexDrew)]}/>
     )}
   </>
 }
 
-type CssPosition = {
-  radius: number
-  direction: number
-  rotation: number
+function isPositionned({direction, radius, rotation}: CssPosition):boolean{
+  return direction !== null && radius !== null && rotation !== null
 }
 
+function isCloverDrew(index:number, animation:Animation<DrawView>|undefined, indexDrew:number):boolean{
+  return animation !== undefined && indexDrew === index
+}
+
+type CssPosition = {
+  radius: number|null
+  direction: number|null
+  rotation: number|null
+} 
+
 const style = ({direction, radius, rotation}: CssPosition) => css`
-  left: ${Math.cos(direction * 2 * Math.PI) * radius * 24 + (16 / 9 * 100 - cloverSize) / 2}em;
-  top: ${Math.sin(direction * 2 * Math.PI) * radius * 24 + 25 + headerHeight + 3}em;
-  transform: rotate(${rotation * 90}deg);
+  left: ${Math.cos(direction! * 2 * Math.PI) * radius! * 24 + (16 / 9 * 100 - cloverSize) / 2}em;
+  top: ${Math.sin(direction! * 2 * Math.PI) * radius! * 24 + 25 + headerHeight + 3}em;
+  transform: rotate(${rotation! * 90}deg);
 `
--- rules/src/moves/DrawClover.ts
+++ rules/src/moves/DrawClover.ts
@@ -1,7 +1,9 @@
 import GameState from '../GameState'
 import GameView from '../GameView'
 import Clover from '../material/Clover'
+import Move from './Move'
 import MoveType from './MoveType'
+import MoveView from './MoveView'
 
 type DrawClover = {
   type: MoveType.DrawClover
@@ -24,4 +26,8 @@ export function drawCloverInView(state: GameView, move: DrawView) {
   const player = state.players[state.activePlayer! - 1]
   state.faceDownClovers--
   player.clovers.push(move.clover)
+}
+
+export function isDrawClover(move: Move | MoveView): move is DrawClover {
+  return move.type === MoveType.DrawClover
 }
