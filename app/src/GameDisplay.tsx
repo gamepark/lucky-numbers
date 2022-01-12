0ba13b8f02c830cd4593061882f8775f2dc116c8
Tho33 <theo.gregorio@laposte.net>
2022-01-12 15:48:11 +0100

    fix ladybug movements

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -3,6 +3,7 @@ import {css, keyframes} from '@emotion/react'
 import GameView from '@gamepark/lucky-number/GameView'
 import {usePlayerId, useTutorial} from '@gamepark/react-client'
 import {Letterbox, Picture} from '@gamepark/react-components'
+import { useEffect, useState } from 'react'
 import DrawPile from './clovers/DrawPile'
 import FaceUpClovers from './clovers/FaceUpClovers'
 import Images from './Images'
@@ -18,6 +19,11 @@ export default function GameDisplay({game}: Props) {
   const playerId = usePlayerId<number>()
   const tutorial = useTutorial()
 
+  const [ladybugPosition, setLadybugPosition] = useState<number>(0)
+  useEffect(() => {
+    if(game.activePlayer !== undefined)
+    setLadybugPosition(ladybugPosition + getLadyBugIncrement(getDisplayPosition(playerId, game.activePlayer-1 , game.players.length), ladybugPosition))
+  },[game.activePlayer])
   return (
     <Letterbox css={letterBoxStyle} top={0}>
       <div css={givePerspective}>
@@ -46,7 +52,7 @@ export default function GameDisplay({game}: Props) {
           nbPlayers={game.players.length}
           />
 
-        {game.activePlayer !== undefined && <Picture src={Images.ladybug} css={[ladybugStyle(getDisplayPosition(playerId, game.activePlayer-1, game.players.length)) ]} />}
+        {game.activePlayer !== undefined && <Picture src={Images.ladybug} css={[ladybugStyle(ladybugPosition) ]} />}
       </div>
 
       {tutorial && <TutorialPopup game={game} tutorial={tutorial}/>}
@@ -55,25 +61,36 @@ export default function GameDisplay({game}: Props) {
   )
 }
 
-const ladybugStyle = (activePlayer:number) => css`
+const ladybugStyle = (ladyBug:number) => css`
 position:absolute;
 filter: drop-shadow(0 0 0.3em black);
 left:87em;
 top:45.4em;
 height:4.5em;
-transition:transform 1s ease-in-out;
+transition:transform 3s ease-in-out;
 
-transform:rotateZ(${getLadyBugRotation(activePlayer)}deg) translateX(-73em);
+transform:rotateZ(${ladyBug}deg) translateX(-73em);
 `
 
-function getLadyBugRotation(index:number):number{
-  switch(index){
-    case 0:return -4
-    case 1:return 4
-    case 2:return 180
-    case 3:return 185
-    default:return 0
+function getLadyBugIncrement(index:number, ladyBug:number):number{
+  if(ladyBug === 0){
+    switch(index){
+      case 0:return -4
+      case 1:return 4
+      case 2:return 180
+      case 3:return 185
+      default:return 0
+    }
+  } else {
+    switch(index){
+      case 0:return 171
+      case 1:return 8
+      case 2:return 176
+      case 3:return 5
+      default:return 0
+    }
   }
+
 }
 
 const fadeIn = keyframes`
