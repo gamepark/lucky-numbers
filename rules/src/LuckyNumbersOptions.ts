83fd72c22d4f29fc519d04c22f0a940d20f2153d
Romain Fromi <romain.fromi@gmail.com>
2022-01-23 18:36:00 +0100

    Restrict variants to games with one subscriber

--- rules/src/LuckyNumbersOptions.ts
+++ rules/src/LuckyNumbersOptions.ts
@@ -15,11 +15,13 @@ export function isGameOptions(arg: GameState | LuckyNumbersOptions): arg is Luck
 export const LuckyNumbersOptionsSpec: OptionsSpec<LuckyNumbersOptions> = {
   isBrunoVariant:{
     label:(t:Function) => t('Bruno Variant'),
-    help:t => t('bruno.variant.help')
+    help:t => t('bruno.variant.help'),
+    subscriberRequired: true
   },
   isMichaelVariant:{
     label:(t:Function) => t('Michael Variant'),
-    help:t => t('michael.variant.help')
+    help:t => t('michael.variant.help'),
+    subscriberRequired: true
   }
 }
 
