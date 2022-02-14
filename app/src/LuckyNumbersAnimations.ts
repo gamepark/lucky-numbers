6ae44c4d96ab4b260648ed12cd4c10e6eef89025
Tho33 <theo.gregorio@laposte.net>
2022-02-14 09:32:09 +0100

    reduce animations durations

--- app/src/LuckyNumbersAnimations.ts
+++ app/src/LuckyNumbersAnimations.ts
@@ -10,11 +10,11 @@ const luckyNumbersAnimations : Animations<GameView, MoveView, number> = {
         const tuto:boolean = state.isTutorial ?? false
         
         if (move.type === MoveType.DrawClover || move.type === MoveType.DrawCloverForEveryone){
-            return 2 + (tuto ? 1 : 0)
+            return 1.5 + (tuto ? 1 : 0)
         } else if (move.type === MoveType.PlaceClover){
             return isBrunoVariantTrigger(state.players[move.playerId-1].garden, move.row, move.column, move.clover, state.isBrunoVariant === true) === true 
-                ? (action.playerId === playerId ? 1.7 : 2.7) 
-                : (action.playerId === playerId ? 0 : 0.7+ (tuto ? 1 : 0))
+                ? (action.playerId === playerId ? 1.2 : 2.2) 
+                : (action.playerId === playerId ? 0 : 0.4+ (tuto ? 1 : 0))
         } 
         return 0
     }
